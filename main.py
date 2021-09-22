a067dbd6e553cef02a5d519bec3629ec2497a598
MorenoRA <58057661+M466-student@users.noreply.github.com>
2021-09-22 20:23:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ print('3.kelvin')
 
 while True:
     # Take input from the user
-    choice = input("Masukan pilihan (1/2/3):")
+    choice = input("Masukan pilihan (1/2/3):")   # input user dari pilihan di atas 
 
-    if choice == '1':
+    if choice == '1': # if user pick number 1. the program will start here
         Celc = float(input('tentukan angka celcius :'))
-        CtoFahr = (9/5 * Celc) + 32
+        CtoFahr = (9/5 * Celc) + 32  # rumus konversi suhu 
         print(CtoFahr, 'fahrenheit')
 
     if choice == '2':
@@ -32,9 +32,13 @@ while True:
 
 
     else:
-        break
+        break # break mean end so after u input the numbers and got number output the script will end 
 
 
+        
+        #notes for cycom members
+        #kalo mau copas silahkan tapi ganti variable nya jadi  suhu lain dulu 
+        
 
 
 
